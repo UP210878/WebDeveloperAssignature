cc3b4fb85855b4bba92f8b28ebedd6e001f04731
UP210878 <up210878@alumnos.upa.edu.mx>
2024-02-22 14:11:02 -0600

    Cont clicks

--- CONT-CLICKS/app.js
+++ CONT-CLICKS/app.js
@@ -1,10 +1,30 @@
-    let a = 1;
-    let b = 2;
-    let c = a+b;
-    alert(c);
+let counterClicks = document.getElementById('counterClicks');
+let btnIncrement = document.getElementById('Increment');
+let btnDecrement = document.getElementById('Decrement');
+let btnReset = document.getElementById('Reset');
+
+let count = 0;
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    counterClicks.innerText = count;
+
+    btnIncrement.addEventListener("click", function () {
+        count++;
+        counterClicks.innerText = count;
+    });
     
+    btnDecrement.onclick = () => {
+        (count < 1 )? 0 : count--;
+        counterClicks.innerText = count;
+    }
+    
+    btnReset.addEventListener("click", () => {
+        count = 0;
+        counterClicks.innerText = count;
+    });
+});
 
-let btn = document.getElementById("btn");
 
-console.log(btn)
+console.log(counterClicks);
+console.log(btnIncrement)
 
