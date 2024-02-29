dd2c1af6e1f3ace100043b711612c4ebe28b032c
UP210878 <up210878@alumnos.upa.edu.mx>
2024-02-29 14:06:34 -0600

    Clock

---
+++ CLOCK-APP/assets/js/app.js
@@ -0,0 +1,54 @@
+// alert("Hola");
+const seconds = document.getElementById("seconds");
+const minutes = document.getElementById("minutes");
+const hours = document.getElementById("hours");
+const formAlarm = document.getElementById("form-alarm");
+let alarmDate;
+
+// function formatNumber(number){
+//   // if (number < 10) {
+//   //   return "0" + number;
+//   // } else {
+//   //   return number;
+//   // }
+//   return (number < 10)? "0" + number : number;
+// }
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  getCurrentTime();
+});
+
+setInterval(()=>{
+  getCurrentTime()
+},1000);
+
+formAlarm.addEventListener('submit', (event)=>{
+  event.preventDefault();
+  
+  const formData = new FormData(event.currentTarget);
+  const value = formData.get("time");
+  if (value === null || value ===""){
+    alert("Establezca una fecha");
+    return;
+  }
+  alarmDate = new Date(value);
+
+});
+
+const formatNumber = (number) => number < 10? "0" + number : number;
+
+const getCurrentTime = () => {
+  const currentDate = new Date();
+  
+  const currentHours = currentDate.getHours();
+  const currentMinutes = currentDate.getMinutes();
+  const currentSeconds = currentDate.getSeconds();
+  
+  console.log(alarmDate);
+  
+  hours.innerText = formatNumber(currentHours);
+  minutes.innerText = formatNumber(currentMinutes);
+  seconds.innerText = formatNumber(currentSeconds); 
+}
+
